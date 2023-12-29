cd35a61f6fc6dd84bd7d4952d9b6df35fbfcef12
Luba Hadunova <luba.godunova2018@gmail.com>
2023-12-29 22:10:13 +0100

    feat: add solution to isSameLength function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -146,8 +146,8 @@ function getAverage(arr) {
  *    isSameLength(['apple', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  return arr.every((element) => element.length === arr[0].length);
 }
 
 /**
