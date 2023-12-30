a781e555f39a6e0e18718622bd9a1e64b734cd3f
Luba Hadunova <luba.godunova2018@gmail.com>
2023-12-30 19:56:32 +0100

    feat: add solution to shiftArray function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -566,8 +566,9 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  const index = (arr.length - n) % arr.length;
+  return arr.slice(index).concat(arr.slice(0, index));
 }
 
 /**
