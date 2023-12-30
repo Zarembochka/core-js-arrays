2839dc14056c0afe7546ce4ba12f26e457843cd6
Luba Hadunova <luba.godunova2018@gmail.com>
2023-12-30 10:47:37 +0100

    feat: add solution to createChunks function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -340,8 +340,12 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  const size = Math.ceil(arr.length / chunkSize);
+  const result = Array.from({ length: size }, (element, index) =>
+    arr.slice(index * chunkSize, index * chunkSize + chunkSize)
+  );
+  return result;
 }
 
 /**
