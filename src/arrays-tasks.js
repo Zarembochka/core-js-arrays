e8045dcbbbdf34f8bed88775742acbe1d24ac9c7
Luba Hadunova <luba.godunova2018@gmail.com>
2023-12-30 16:10:15 +0100

    feat: add solution to findCommonElements function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -500,8 +500,9 @@ function getMaxItems(arr, n) {
  *    findCommonElements(['a', 'b', 'c'], ['b', 'c', 'd']) => [ 'b', 'c' ]
  *    findCommonElements([1, 2, 3], ['a', 'b', 'c']) => []
  */
-function findCommonElements(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function findCommonElements(arr1, arr2) {
+  const result = arr1.filter((element) => arr2.includes(element) === true);
+  return result;
 }
 
 /**
